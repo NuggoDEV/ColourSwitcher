c7a45fe8a8e6f1cdeb4b8f14b64614dded607b89
Nuggo <NuggoDEV@gmail.com>
2022-12-02 17:06:25 +1030

    UI should be like fully complete now, couple changes to default values soon:tm:

--- ColourSwitcher/Configuration/Config.cs
+++ ColourSwitcher/Configuration/Config.cs
@@ -12,7 +12,40 @@ namespace ColourSwitcher
     {
         public static Config Instance { get; set; }
 
+        public Color leftSaberColour = Color.white;
+        public Color rightSaberColour = Color.white;
+        public Color wallColour = Color.white;
         public Color bombColour = Color.white;
+
+        public bool timedSwitch1Bool = false;
+        public bool timedSwitch2Bool = false;
+        public bool timedSwitch3Bool = false;
+        public bool timedSwitch4Bool = false;
+
+        public float timedSwitch1Time = 0;
+        public float timedSwitch2Time = 0;
+        public float timedSwitch3Time = 0;
+        public float timedSwitch4Time = 0;
+
+        public Color leftSaberColour1 = Color.white;
+        public Color rightSaberColour1 = Color.white;
+        public Color bombColour1 = Color.white;
+        public Color wallColour1 = Color.white;
+
+        public Color leftSaberColour2 = Color.white;
+        public Color rightSaberColour2 = Color.white;
+        public Color bombColour2 = Color.white;
+        public Color wallColour2 = Color.white;
+
+        public Color leftSaberColour3 = Color.white;
+        public Color rightSaberColour3 = Color.white;
+        public Color bombColour3 = Color.white;
+        public Color wallColour3 = Color.white;
+
+        public Color leftSaberColour4 = Color.white;
+        public Color rightSaberColour4 = Color.white;
+        public Color bombColour4 = Color.white;
+        public Color wallColour4 = Color.white;
 
 
 
--- ColourSwitcher/UI/ColourSwitcherViewController.cs
+++ ColourSwitcher/UI/ColourSwitcherViewController.cs
@@ -83,18 +83,134 @@ namespace ColourSwitcher.UI
     {
         Config config = Config.Instance;
 
-        static bool isAprilFirst = (DateTime.Now.Month == 4) && (DateTime.Now.Day == 1);
-        static bool __true = true;
-
         void ClearBombColour()
         {
             bombColour = BombColour.defaultColour;
             NotifyPropertyChanged("bombColour");
         }
+
+
+        bool timedSwitch1Bool
+        {
+            get => config.timedSwitch1Bool; set => config.timedSwitch1Bool = value;
+        }
+        bool timedSwitch2Bool
+        {
+            get => config.timedSwitch2Bool; set => config.timedSwitch2Bool = value;
+        }
+        bool timedSwitch3Bool
+        {
+            get => config.timedSwitch3Bool; set => config.timedSwitch3Bool = value;
+        }
+        bool timedSwitch4Bool
+        {
+            get => config.timedSwitch4Bool; set => config.timedSwitch4Bool = value;
+        }
+
+        float timedSwitch1Time
+        {
+            get => config.timedSwitch1Time; set => config.timedSwitch1Time = value;
+        }
+        float timedSwitch2Time
+        {
+            get => config.timedSwitch2Time; set => config.timedSwitch2Time = value;
+        }
+        float timedSwitch3Time
+        {
+            get => config.timedSwitch3Time; set => config.timedSwitch3Time = value;
+        }
+        float timedSwitch4Time
+        {
+            get => config.timedSwitch4Time; set => config.timedSwitch4Time = value;
+        }
+
+        Color leftSaberColour
+        {
+            get => config.leftSaberColour; set => config.leftSaberColour = value;
+        }
+        Color rightSaberColour
+        {
+            get => config.rightSaberColour; set => config.rightSaberColour = value;
+        }
+        Color wallColour
+        {
+            get => config.wallColour; set => config.wallColour = value;
+        }
         Color bombColour
         {
             get => config.bombColour;
             set => config.bombColour = value.ColorWithAlpha(1);
+        }
+
+        Color leftSaberColour1
+        {
+            get => config.leftSaberColour1; set => config.leftSaberColour1 = value;
+        }
+        Color rightSaberColour1
+        {
+            get => config.rightSaberColour1; set => config.rightSaberColour1 = value;
+        }
+        Color bombColour1
+        {
+            get => config.bombColour1; set => config.bombColour1 = value;
+        }
+        Color wallColour1
+        {
+            get => config.wallColour1; set => config.wallColour1 = value;
+        }
+
+
+        Color leftSaberColour2
+        {
+            get => config.leftSaberColour2; set => config.leftSaberColour2 = value;
+        }
+        Color rightSaberColour2
+        {
+            get => config.rightSaberColour2; set => config.rightSaberColour2 = value;
+        }
+        Color bombColour2
+        {
+            get => config.bombColour2; set => config.bombColour2 = value;
+        }
+        Color wallColour2
+        {
+            get => config.wallColour2; set => config.wallColour2 = value;
+        }
+
+
+        Color leftSaberColour3
+        {
+            get => config.leftSaberColour3; set => config.leftSaberColour3 = value;
+        }
+        Color rightSaberColour3
+        {
+            get => config.rightSaberColour3; set => config.rightSaberColour3 = value;
+        }
+        Color bombColour3
+        {
+            get => config.bombColour3; set => config.bombColour3 = value;
+        }
+        Color wallColour3
+        {
+            get => config.wallColour3; set => config.wallColour3 = value;
+        }
+
+
+        Color leftSaberColour4
+        {
+            get => config.leftSaberColour4; set => config.leftSaberColour4 = value;
+        }
+        Color rightSaberColour4
+        {
+            get => config.rightSaberColour4; set => config.rightSaberColour4 = value;
+        }
+        Color bombColour4
+        {
+            get => config.bombColour4; set => config.bombColour4 = value;
+        }
+        Color wallColour4
+        {
+            get => config.wallColour4; set => config.wallColour4 = value;
         }
 
 
